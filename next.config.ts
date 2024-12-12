21aa17049073eca479fdde7888bf075d2639699a
Mhar Andal <mhar@codeverse.com>
2024-12-12 15:28:17 -0600

    config: add replicate image pattern

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'replicate.com',
+        protocol: 'https',
+      },
+      {
+        hostname: 'replicate.delivery',
+        protocol: 'https',
+      },
+    ],
+  },
   /* config options here */
   serverExternalPackages: ['knex'],
 }
