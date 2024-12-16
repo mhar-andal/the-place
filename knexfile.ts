b6d5812ca5569383eae3aca8767d1204fbf04848
Mhar Andal <mhar@codeverse.com>
2024-12-15 23:45:59 -0600

    chore(db): add production config

--- knexfile.ts
+++ knexfile.ts
@@ -10,4 +10,8 @@ export default {
     },
     useNullAsDefault: true,
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+  },
 }
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,10 @@
 import knexfile from '../../knexfile'
 
 // eslint-disable-next-line
-const knex = require('knex')(knexfile.development)
+const knex = require('knex')(
+  process.env.NODE_ENV === 'development'
+    ? knexfile.development
+    : knexfile.production,
+)
 
 export default knex
